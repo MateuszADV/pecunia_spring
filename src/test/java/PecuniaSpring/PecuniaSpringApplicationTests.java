e8394d214dc47ad7e32395eebf718852099e7f4d
MateuszADV <mateusz.adv@gmail.com>
2023-07-11 18:57:16 +0200

    Poprawka testy

--- src/test/java/PecuniaSpring/PecuniaSpringApplicationTests.java
+++ src/test/java/PecuniaSpring/PecuniaSpringApplicationTests.java
@@ -1,13 +1,11 @@
 package PecuniaSpring;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PecuniaSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
