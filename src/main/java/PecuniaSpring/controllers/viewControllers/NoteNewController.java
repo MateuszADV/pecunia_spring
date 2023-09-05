ae5e69fca9b42e186e32e752a2b9326be76276a2
MateuszADV <mateusz.adv@gmail.com>
2023-09-05 13:28:58 +0200

    Dodano Wyświetlanie nowych banknotów

--- src/main/java/PecuniaSpring/controllers/viewControllers/NoteNewController.java
+++ src/main/java/PecuniaSpring/controllers/viewControllers/NoteNewController.java
@@ -1,6 +1,7 @@
 package PecuniaSpring.controllers.viewControllers;
 
 import PecuniaSpring.models.repositories.NoteRepository;
+import PecuniaSpring.models.sqlClass.CountryByStatus;
 import PecuniaSpring.models.sqlClass.GetNotesByStatus;
 import lombok.AllArgsConstructor;
 import org.apache.catalina.LifecycleState;
@@ -8,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import utils.JsonUtils;
 
 import java.util.ArrayList;
@@ -21,17 +23,39 @@ public class NoteNewController {
 
     @GetMapping("/note/newNotes")
     public String getIndex(ModelMap modelMap) {
-        List<Object[]> objects = noteRepository.getNotesByStatus("NEW");
-        List<GetNotesByStatus> getNotesByStatuses = new ArrayList<>();
-        for (Object[] object : objects) {
-            getNotesByStatuses.add(new ModelMapper().map(object[0],GetNotesByStatus.class));
+        List<Object[]> objects;
+        List<CountryByStatus> countryByStatusList = new ArrayList<>();
+        objects = noteRepository.countryByStatus("NEW");
+        try {
+            System.out.println(JsonUtils.gsonPretty(objects));
+            for (Object[] object : objects) {
+                countryByStatusList.add(new ModelMapper().map(object[0], CountryByStatus.class));
+            }
+
+            modelMap.addAttribute("countryByStatusList", countryByStatusList);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println("PPPPPPPPPPPPPPPPPPPPPPPPPPP");
-        System.out.println(objects.size());
-        System.out.println(JsonUtils.gsonPretty(getNotesByStatuses));
-        System.out.println("PPPPPPPPPPPPPPPPPPPPPPPPPPP");
 
         return "note/newNote/index";
+    }
+
+    @GetMapping("/note/newNote/country/notes/{countryId}")
+    public String getCountryNotes(@PathVariable("countryId") Long countryId, ModelMap modelMap) {
+        System.out.println("[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]C");
+        System.out.println(countryId);
+        System.out.println("[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]C");
+
+        List<Object[]> objects;
+        objects = noteRepository.getNotesByStatus("NEW", countryId);
+        List<GetNotesByStatus> getNotesByStatusList = new ArrayList<>();
+        for (Object[] object : objects) {
+            getNotesByStatusList.add(new ModelMapper().map(object[0],GetNotesByStatus.class));
+        }
+
+        modelMap.addAttribute("newNotesList", getNotesByStatusList);
+        System.out.println(JsonUtils.gsonPretty(getNotesByStatusList));
+        return "note/newNote/notes";
     }
 
     @GetMapping("/note/newNotes/loc")
